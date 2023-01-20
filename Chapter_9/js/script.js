7e0222f41e14bc7a68e0c96a9cd1f495647a1b9a
KubrikKY <kubrikkirill@gmail.com>
2023-01-20 19:24:40 +0300

    Complete Promise chapter

--- Chapter_9/js/script.js
+++ Chapter_9/js/script.js
@@ -2,12 +2,56 @@
 
 // -----------------Promise------------------
 
-let promise = new Promise (function(resolve, reject) {
-    // executor
-});
+// let promise = new Promise (function(resolve, reject) {
+//     // executor
+// });
 
 function delay(ms) {
     return new Promise (resolve => setTimeout(resolve, ms));
   }
   
   delay(3000).then(() => console.log('выполнилось через 3 секунды'));
+
+
+//   --------------Промисы: обработка ошибок----------------
+
+  let promise = new Promise ((resolve, reject) => {
+    setTimeout(() => {reject(new Error('whoooooops!!'));}, 2000);
+    setTimeout(() => {resolve('some value')}, 1000);
+  }).then(value => console.log(value))
+  .catch(err => console.log(err.message + '!!???'));
+
+// ----------------Промисификация-----------------------
+
+function loadScript (src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error('Ошибка загрузки ' + src));
+}
+
+let loadScriptNew = function (src) {
+    return new Promise ((resolve, reject) => {
+        loadScript(src, (err, script) => {
+            if (err) reject(err)
+            else resolve(script);
+        });
+    });
+}
+
+function promisify (f) {
+    return function(...args) {
+        return new Promise((resolve, reject) => {
+            function callback(err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+
+            args.push(callback);
+            f.call(this, ...args);
+        });
+    };
+}
